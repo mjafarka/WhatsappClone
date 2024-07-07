5af98d8b3b15b67cba5bd4141a13ffcfc39b20b0
mjafarka <muhammedjafarka@gmail.com>
2024-07-07 18:50:28 +0530

    Update index.js

--- src/mainComponents/sidebar/main/index.js
+++ src/mainComponents/sidebar/main/index.js
@@ -2,16 +2,29 @@ import React from 'react'
 import './index.scss'
 import Search from '../search';
 import UserProfile from '../userProfile';
+import SearchResult from '../searchResults';
+import { SearchProvider } from '../../../context/SearchContext';
 
 function SideBar() {
 
   const profiles = []
   for (let i = 0 ; i < 10 ; i ++ ){
-    profiles.push(<UserProfile/>)
+    profiles.push(<UserProfile user={{emaidId: "muhammed@gmail.com",profileLoc
+      : 
+      "jdksjkfsl",
+      userId
+      : 
+      "89faueio",
+      userName
+      : 
+      "muhammed"}} key={i}/>)
   }
   return (
     <div className='sideBar'>
-            <Search/>
+          <SearchProvider> {/* to search local and db profiles */}
+              <Search/>
+              <SearchResult/>
+          </SearchProvider>
             {profiles}
     </div>
   )
