484294014e0fe557c2f4eac68e10abd8d538baa3
mjafarka <muhammedjafarka@gmail.com>
2024-07-07 18:50:50 +0530

    Update index.js

--- src/mainComponents/sidebar/search/index.js
+++ src/mainComponents/sidebar/search/index.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.scss'
 import { CiSearch } from "react-icons/ci";
+import { useSearchMethodDispatcherContext, useSearchResult } from '../../../context/SearchContext';
 
 function Search() {
+
+  const searchTrigger = useSearchMethodDispatcherContext();
+  const [subName, setSubName] = useState("");
+
+  function searchPeople () {
+      searchTrigger({type: 'recent', subName: subName});
+  }
+
   return (
     <>
       <div className='searchContainer'>
-        <input type='text' placeholder='Search...' className='input-search' />
+        <input type='text' placeholder='Search...' className='input-search' 
+        onChange={e => setSubName(e.target.value)}/>
         <span className='search-icon'>
-          <CiSearch />
+          <CiSearch onClick={() => searchPeople()}/>
         </span>
       </div>
     </>
