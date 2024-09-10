b40d9d40fc447762128e43e9cfbc95b817ac2c57
jafar <mjafer657@gmail.com>
2024-09-10 13:11:15 +0530

    message-order-and-online-offline-search

--- src/mainComponents/sidebar/search/index.js
+++ src/mainComponents/sidebar/search/index.js
@@ -2,6 +2,9 @@ import React, { useContext, useEffect, useState } from 'react'
 import './index.scss'
 import { CiSearch } from "react-icons/ci";
 import { defaultContext, subNameContext, useSearchMethodDispatcherContext, useSearchResult } from '../../../context/SearchContext';
+import { getChatHistoryDoc } from '../../../firebase/firebaseDB';
+import { useUser } from '../../../context/UserContext';
+import { getRecentChatUsers } from '../../../firebase/helpers';
 
 function Search() {
 
@@ -10,15 +13,18 @@ function Search() {
   const [subNameSearch, setSubNameSearch] = useState(subName);
   const {setSubNameInContext} = useContext(subNameContext);
   const {setShowSearchResult} = useContext(defaultContext);
+  const user = useUser();
 
   useEffect(() => {
     setSubNameSearch(subName);
   },[subName])
 
-  function searchPeople () {
-      searchTrigger({type: 'recent', subName: subNameSearch});
-      setSubNameInContext(subNameSearch);
-      setShowSearchResult(true);
+  async function searchPeople () {
+    const recentHistoryRef = await getChatHistoryDoc(user.userId);
+    searchTrigger({type: 'recent', subName: subNameSearch, recentHistoryDoc:
+    recentHistoryRef});
+    setSubNameInContext(subNameSearch);
+    setShowSearchResult(true);
   }
 
   return (
